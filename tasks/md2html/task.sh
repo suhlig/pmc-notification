45d5897cc95c655b9cfde98afd575a2b1133cc6b
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-05-10 20:23:08 +0200

    Run git from within the markdown directory

--- tasks/md2html/task.sh
+++ tasks/md2html/task.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ex
 
 bundle install --jobs 4
-git diff-tree --no-commit-id --name-only -r HEAD | xargs bundle exec pipeline/tasks/md2html/md2html.rb
+
+pushd markdown > /dev/null
+  git diff-tree --no-commit-id --name-only -r HEAD | xargs -I {} readlink -f {} > latest.txt
+popd > /dev/null
+
+xargs bundle exec pipeline/tasks/md2html/md2html.rb < markdown/latest.txt
