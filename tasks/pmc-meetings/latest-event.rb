99d68f9485084b371f3f6e4a740df32417af2b52
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2017-05-11 13:01:08 +0200

    WIP Add fictional resource to pipeline

--- tasks/pmc-meetings/latest-event.rb
+++ tasks/pmc-meetings/latest-event.rb
@@ -1,22 +1,56 @@
 #!/usr/bin/env ruby
 
+#
+# If an event took place in the given calendar
+#
+
 require 'http'
 require 'icalendar/recurrence'
 
-response = HTTP
-  .follow
-  .get('https://www.google.com/calendar/ical/cloudfoundry.org_8ms13q67p9jjeeilng6dosnu50@group.calendar.google.com/public/basic.ics')
+def day(day)
+  beginning = Time.utc(day.year, day.month, day.day,  0,  0,  0)
+  ending    = Time.utc(day.year, day.month, day.day, 23, 59, 59)
+  beginning..ending
+end
+
+def month(year, month)
+  beginning = Time.utc(year, month,  1,  0,  0,  0)
+  ending    = Time.utc(year, month, 31, 23, 59, 59)
+  beginning..ending
+end
+
+def year(year)
+  beginning = Time.utc(year,  1,  1,  0,  0,  0)
+  ending    = Time.utc(year, 12, 31, 23, 59, 59)
+  beginning..ending
+end
+
+def today
+  day(Time.now.utc)
+end
+
+def yesterday
+  day(Time.now.utc - 60 * 60 * 24)
+end
+
+def this_month
+  now = Time.now.utc
+  month(now.year, now.month)
+end
+
+def this_year
+  year(Time.now.year)
+end
+
+response = HTTP.follow.get()
 
 cals = Icalendar::Calendar.parse(response)
 
-# materialize recurrences in this year
-cals.first.events.map do |event|
-  event.occurrences_between(Time.parse("2017-01-01"), Time.parse("2017-12-31")).map do |e|
+range = month(2017, 3)
 
-    # yesterday?
-    if 5 == e.start_time.month && 3 == e.start_time.day
-      "#{event.summary}: #{e.start_time} - #{e.end_time}"
-    end
+cals.first.events.map do |event|
+  event.occurrences_between(range.begin, range.end).map do |e|
+    "#{event.summary}: #{e.start_time} - #{e.end_time}"
   end
 end.flatten.compact.sort.each do |e|
   puts e
